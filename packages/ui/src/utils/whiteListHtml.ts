ce8508619c846d276969a89888ec3fbddd2e9f3d
jyeongpark <jiyeongstar@gmail.com>
2025-08-20 13:17:56 +0900

    feat(ui): dangerouslySetInnerHTML에 안정성을 추가하기 위한 유틸 함수 생성

---
+++ packages/ui/src/utils/whiteListHtml.ts
@@ -0,0 +1,7 @@
+// 아주 라이트한 필터: strong, b, em, i, u, a 만 허용
+
+export function whitelistHtml(html: string) {
+  return html
+    .replace(/<(?!\/?(strong|b|em|i|u|a)(\s|>|\/))/gi, "&lt;")
+    .replace(/javascript:/gi, "");
+}
