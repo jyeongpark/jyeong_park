908323691204f6ee3b0f346d618567d1ffbc6892
jyeongpark <jiyeongstar@gmail.com>
2025-08-26 14:51:25 +0900

    fix(ui): DateText에 duration 추가해 해당 기간을 나타낸다.

--- packages/ui/src/components/DateText/DateText.tsx
+++ packages/ui/src/components/DateText/DateText.tsx
@@ -4,18 +4,21 @@ import { DateTextRoot } from "./DateText.style";
 export type DateTextProps = React.HTMLAttributes<HTMLDivElement> & {
   startDate: string;
   endDate?: string;
+  duration?: string;
   size?: "sm" | "md";
 };
 
 export const DateText = ({
   startDate,
   endDate = "현재",
+  duration,
   size = "sm",
   ...rest
 }: DateTextProps) => {
   return (
     <DateTextRoot data-cy="DateText" $size={size} {...rest}>
-      ({startDate} ~ {endDate})
+      ({startDate} - {endDate}
+      {duration ? ` / ${duration}` : ""})
     </DateTextRoot>
   );
 };
